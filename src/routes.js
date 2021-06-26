6eeb507f177f82cbb8a1567d017feb659e415f1f
Leticia Dias <contatoleticiadia@gmail.com>
2021-06-26 13:40:53 -0300

    feat: add routes for application

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
+const routes = express.Router()
+
+
+routes.get('/', (req, res) => res.render('index', {page: 'enter-room'}))
+routes.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'}))
+
+routes.post('/create-room', RoomController.create)
+routes.get('/room/:room', RoomController.open)
+routes.post('/enterroom', RoomController.enter)
+
+routes.post('/question/create/:room', QuestionController.create)
+routes.post('/question/:room/:question/:action', QuestionController.index)
+
+
+
+module.exports = routes
